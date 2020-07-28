ad77cb81a4223daeff61f48711da4016b3ae5b8f
Cristiian <credrope@hotmail.com>
2020-07-28 11:34:49 -0500

    Detalles pagina principal

--- js/app.js
+++ js/app.js
@@ -8,17 +8,14 @@ $(document).ready(function () {
     $(".categ").mouseenter(function () {
         $(this).css("background-color", "white");
         $(this).css("color", "rgb(16, 36, 83)");
-        $(this).css("box-shadow", "0px 5px 9px 0 rgba(255, 255, 255, 0.5), 0px 7px 20px 0 rgba(255, 255, 255, 0.39)");
-        $(this).css("z-index", "1");
+      
         
     });
     
-    // Funcion para darle animacion Categorias Populares cuando entra el cursor
+    // Funcion para darle animacion Categorias Populares cuando sale el cursor
     $(".categ").mouseleave(function () {
         $(this).css("background-color", "rgb(16, 36, 83)");
         $(this).css("color", "white");
-        $(this).css("box-shadow", "none");
-        $(this).css("z-index", "none");
         
     }); 
     
